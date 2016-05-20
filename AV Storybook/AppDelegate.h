8ff3d8c62ba84b8a5e1ffe7f0ed74b96b3eaae91
moiseevanton <athidif94@gmail.com>
2016-05-20 15:13:55 -0400

    Initial Commit

---
+++ AV Storybook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AV Storybook
+//
+//  Created by Anton Moiseev on 2016-05-20.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AV Storybook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AV Storybook
+//
+//  Created by Anton Moiseev on 2016-05-20.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
